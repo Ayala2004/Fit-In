0ec399aa3682cc3964957ee33fccd86e4538747c
Ayala Elmaleh <ayalaelmaleh4@gmail.com>
2025-12-25 15:33:00 +0200

    feat: login with bcrypt and middleware protection is working

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,7 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-import { encrypt } from '@/lib/auth'; // ייבוא הפונקציה החדשה
+import { encrypt } from '@/lib/auth';
 import { cookies } from 'next/headers';
+import bcrypt from 'bcrypt'; // ייבוא bcrypt
 
 export async function POST(req: Request) {
   try {
@@ -11,27 +12,29 @@ export async function POST(req: Request) {
       where: { email },
     });
 
-    // הערה: בעתיד כדאי להשתמש ב-bcrypt להשוואת סיסמאות מוצפנות
-    if (!user || user.password !== password) {
+    // שימוש ב-bcrypt להשוואת הסיסמה המוצפנת
+    const isPasswordValid = user ? await bcrypt.compare(password, user.password) : false;
+
+    if (!user || !isPasswordValid) {
       return NextResponse.json({ message: 'פרטים שגויים' }, { status: 401 });
     }
 
     // יצירת הסשן
-    const expires = new Date(Date.now() + 2 * 60 * 60 * 1000); // שעתיים מהיום
+    const expires = new Date(Date.now() + 2 * 60 * 60 * 1000);
     const session = await encrypt({ 
       id: user.id, 
       roles: user.roles, 
       name: `${user.firstName} ${user.lastName}` 
     });
 
-    
-    const cookieStore = await cookies(); // מחכים ל-cookies
+    const cookieStore = await cookies();
     cookieStore.set('session', session, { 
       expires, 
       httpOnly: true, 
       secure: process.env.NODE_ENV === 'production',
       path: '/',
     });
+
     return NextResponse.json({
       id: user.id,
       name: `${user.firstName} ${user.lastName}`,
